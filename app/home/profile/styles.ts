1315df54f0bdfc848aef7bfbb0a4093bee2945c0
RedStar6 <richmondnkrumah04@gmail.com>
2023-09-26 16:59:20 +0000

    added the order card styles

---
+++ app/home/profile/styles.ts
@@ -0,0 +1,20 @@
+import { StyleSheet } from "react-native";
+
+const orders = StyleSheet.create({
+    filters:{
+        backgroundColor:'red',
+        borderRadius:20,
+        borderWidth:1,
+        paddingLeft:15,
+        paddingRight:15,
+        paddingTop:10,
+        paddingBottom:10
+    },
+    filtercontainer:{
+        gap:10
+    }
+})
+
+export {
+    orders
+}
