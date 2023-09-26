7e1fcc6d4d82c7000c87fb90ecfa4aa97dc6ab28
RedStar6 <richmondnkrumah04@gmail.com>
2023-09-26 17:50:23 +0000

    modified:   app/home/profile/Settings.jsx

--- app/home/profile/Settings.jsx
+++ app/home/profile/Settings.jsx
@@ -5,6 +5,28 @@ const Settings = () => {
   return (
     <View>
       <Text>Settings</Text>
+      <View>
+        <Text>Personal information</Text>
+      </View>
+      <View>
+        <View>
+          <Text>Password</Text>
+          <Text>change</Text>
+        </View>
+
+      </View>
+      <View>
+        <Text>Notifications</Text>
+        <View>
+          <Text>Sales</Text>
+        </View>
+        <View>
+          <Text>New Arrivals</Text>
+        </View>
+        <View>
+          <Text>Delivery status changes</Text>
+        </View>
+      </View>
     </View>
   )
 }
