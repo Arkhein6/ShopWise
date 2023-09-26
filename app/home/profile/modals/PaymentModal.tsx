207f9568287dd2a8c11c8b497958533ad9ac8790
RedStar6 <richmondnkrumah04@gmail.com>
2023-09-26 00:44:57 +0000

    modified:   app/home/profile/modals/PaymentModal.tsx

--- app/home/profile/modals/PaymentModal.tsx
+++ app/home/profile/modals/PaymentModal.tsx
@@ -4,18 +4,12 @@ import { Link, router } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 
 const PaymentModal = () => {
-	const isPresented: boolean = router.canGoBack();
-    console.log(isPresented)
 
 	return (
-		<View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}
-		>	{!isPresented && (
-				<Link href="../">
-					<Text style={{
-                        fontSize:40, color:'red'
-                    }}>Dismiss</Text>
-				</Link>
-			)}
+		<View style={{
+			alignItems: 'center', justifyContent: 'center', }}
+		>	
+			<Text>damn</Text>
 			<StatusBar style="dark" />
 		</View>
 	);
