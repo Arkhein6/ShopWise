266510bfa7bdda712e9cde7f329d48484b9ed8a3
RedStar6 <richmondnkrumah04@gmail.com>
2023-09-27 22:19:05 +0000

    #component: added buttons and switches

--- app/home/profile/Settings.jsx
+++ app/home/profile/Settings.jsx
@@ -1,34 +1,61 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, Pressable } from "react-native";
+import React from "react";
+import { Switch } from "react-native";
+import { settings as styles } from "./styles";
 
 const Settings = () => {
-  return (
-    <View>
-      <Text>Settings</Text>
-      <View>
-        <Text>Personal information</Text>
-      </View>
-      <View>
-        <View>
-          <Text>Password</Text>
-          <Text>change</Text>
-        </View>
+	return (
+		<View style={styles.container}>
+      <View style={styles.inner}>
 
+			<Text style={styles.title}>Settings</Text>
+			<View>
+				<View style={styles.subtitlerange}>
+					<Text style={styles.subtitle}>Personal information</Text>
+					<Pressable>
+						<Text style={styles.graytext}>Change</Text>
+					</Pressable>
+				</View>
+				<View style={styles.output}>
+					<Text style={[styles.uppertext,styles.graytext]}>Full Name</Text>
+					<Text>Martha Brown</Text>
+				</View>
+				<View style={styles.output}>
+					<Text style={[styles.uppertext,styles.graytext]}>Date of Birth</Text>
+					<Text>12/12/1989</Text>
+				</View>
+			</View>
+			<View>
+				<View style={styles.subtitlerange}>
+					<Text style={styles.subtitle}>Password</Text>
+					<Pressable>
+						<Text style={styles.graytext}>Change</Text>
+					</Pressable>
+				</View>
+				<View style={styles.output}>
+					<Text style={[styles.uppertext,styles.graytext]}>Password</Text>
+					<Text>*********</Text>
+				</View>
+			</View>
+			<View style={styles.notificationsrange}>
+				<Text style={styles.subtitle}>Notifications</Text>
+				<View style={styles.switchrange}>
+					<Text>Sales</Text>
+					<Switch></Switch>
+				</View>
+				<View style={styles.switchrange}> 
+					<Text>New Arrivals</Text>
+					<Switch></Switch>
+				</View>
+				<View style={styles.switchrange}>
+					<Text>Delivery status changes</Text>
+					<Switch></Switch>
+				</View>
+			</View>
+      
       </View>
-      <View>
-        <Text>Notifications</Text>
-        <View>
-          <Text>Sales</Text>
-        </View>
-        <View>
-          <Text>New Arrivals</Text>
-        </View>
-        <View>
-          <Text>Delivery status changes</Text>
-        </View>
-      </View>
-    </View>
-  )
-}
+		</View>
+	);
+};
 
-export default Settings
+export default Settings;
