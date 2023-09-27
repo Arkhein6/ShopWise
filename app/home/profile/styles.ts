c03c86f756cd86eb9a7b9c9efe41fc5989917329
RedStar6 <richmondnkrumah04@gmail.com>
2023-09-27 22:19:22 +0000

    #styles: added new styles

--- app/home/profile/styles.ts
+++ app/home/profile/styles.ts
@@ -1,26 +1,65 @@
 import { StyleSheet } from "react-native";
 
 const orders = StyleSheet.create({
-    maintitle:{
-        fontSize:40,
-        fontWeight:'bold'
+	maintitle: {
+		fontSize: 40,
+		fontWeight: "bold",
+	},
+	filters: {
+		backgroundColor: "white",
+		borderRadius: 20,
+		paddingLeft: 15,
+		paddingRight: 15,
+		paddingTop: 10,
+		paddingBottom: 10,
+	},
+	filtercontainer: {
+		gap: 10,
+	},
+	currentfilter: {
+		backgroundColor: "black",
+	},
+});
+const settings = StyleSheet.create({
+	container: {
+		alignItems: "center",
+		justifyContent: "center",
+	},
+	inner: {
+		width: "90%",
+	},
+	switchrange: {
+		flexDirection: "row",
+		justifyContent: "space-between",
+		alignItems: "center",
+	},
+	notificationsrange: {
+		gap: 10,
+	},
+	subtitle: {
+		fontWeight: "bold",
+	},
+	title: {
+		fontWeight: "bold",
+		fontSize: 30,
+	},
+	output: {
+		height: 60,
+		elevation: 2,
+		paddingLeft: 15,
+		backgroundColor: "white",
+        justifyContent:'center',
+        gap:2
+	},
+	uppertext: {
+		fontSize: 12,
+	},
+	subtitlerange:{
+        flexDirection:'row',
+        justifyContent:'space-between'
     },
-    filters:{
-        backgroundColor:'white',
-        borderRadius:20,
-        paddingLeft:15,
-        paddingRight:15,
-        paddingTop:10,
-        paddingBottom:10
-    },
-    filtercontainer:{
-        gap:10
-    },
-    currentfilter:{
-        backgroundColor:'black',
+    graytext:{
+        color:'gray'
     }
-})
-
-export {
-    orders
-}
+});
+export { orders, settings };
