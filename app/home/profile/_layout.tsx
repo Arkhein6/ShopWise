46176230d7239fd12df72f4b6253426fe1ade660
RedStar6 <richmondnkrumah04@gmail.com>
2023-09-28 22:30:25 +0000

    added config for stack screens for route profile

--- app/home/profile/_layout.tsx
+++ app/home/profile/_layout.tsx
@@ -5,8 +5,30 @@ import { Stack } from 'expo-router'
 const _layout = () => {
   return (
     <Stack screenOptions={{
-      headerShown:false
     }}>
+      <Stack.Screen name='index' options={{
+        headerShown:false
+      }}/>
+      <Stack.Screen name='Payment' options={{
+        headerTitle:'Payment methods',
+        headerStyle:{
+          backgroundColor:'#F5F5F5'
+        },
+        headerBackVisible:true
+
+      }}/>
+      <Stack.Screen name='ShippingAddresses' options={{
+        headerTitle:'Shipping Addresses',
+        headerStyle:{
+          backgroundColor:'#F5F5F5'
+        },
+        headerBackVisible:true
+        
+      }}/>
+       <Stack.Screen name='Settings' options={{
+        headerShown:false
+      }}/>
+
     </Stack>
   )
 }
