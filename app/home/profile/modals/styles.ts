d0e1f8c430d49c96cb01575441d8df9d9494e81e
RedStar6 <richmondnkrumah04@gmail.com>
2023-09-28 22:31:34 +0000

    added styles

--- app/home/profile/modals/styles.ts
+++ app/home/profile/modals/styles.ts
@@ -73,4 +73,70 @@ const paymentModal = StyleSheet.create({
 	},
 });
 
-export { paymentModal };
+const addShippingAddress = StyleSheet.create({
+	modaloverlay: { 
+        flex: 1,opacity:0.2,
+        backgroundColor: "black", 
+
+    },
+	container: {
+		width:'90%',
+		gap:20
+
+	},
+	title: {
+		fontWeight: "bold",
+		fontSize: 25,
+		textAlign: "center",
+        paddingTop:20
+	},
+	input: {
+		flex: 1,
+	},
+	textInput: {
+		backgroundColor: "white",
+		flexDirection: "row",
+		height: 60,
+		alignItems: "center",
+		position: "relative",
+		paddingLeft: 10,
+		elevation: 2,
+	},
+	textInputs: {
+		rowGap: 20,
+	},
+	inputConfirm: {
+		position: "absolute",
+		right: 10,
+	},
+	button: {
+		backgroundColor: "red",
+		height: 50,
+		alignItems: "center",
+		justifyContent: "center",
+		borderRadius: 30,
+		marginBottom: 30,
+	},
+	disabledButton: {
+		backgroundColor: "gray",
+	},
+	invalidBox: {
+		borderColor: "red",
+		borderWidth: 1,
+	},
+	invalidText: {
+		position: "absolute",
+		bottom: -5,
+		color: "red",
+	},
+	invalidSignUpText: {
+		position: "absolute",
+		bottom: -17,
+		color: "red",
+		fontSize: 12,
+	},
+});
+
+
+export { paymentModal,
+addShippingAddress };
