04c3b4f803822be8a4de75a27355123aeb2383f9
RedStar6 <richmondnkrumah04@gmail.com>
2023-09-28 03:04:27 +0000

    #component: added the update info modal

--- app/home/profile/modals/PersonalInformationChangeModal.tsx
+++ app/home/profile/modals/PersonalInformationChangeModal.tsx
@@ -1,12 +1,252 @@
-import { View, Text,Modal } from 'react-native'
-import React from 'react'
+import {
+	View,
+	Text,
+	Modal,
+	Pressable,
+	ScrollView,
+	TextInput,
+} from "react-native";
+import React, { ReactNode } from "react";
+import { Formik } from "formik";
+import * as Yup from "yup";
+import { Router, useRouter } from "expo-router";
+import { Ionicons, Entypo, AntDesign, FontAwesome } from "@expo/vector-icons";
+import { paymentModal as styles } from "./styles";
 
-const PersonalInformationChangeModal = () => {
-  return (
-    <Modal>
 
-    </Modal>
-  )
-}
+const LoginSchema = Yup.object().shape({
+	name: Yup.string().required("name is required"),
+	expireDate: Yup.string().required("it is required").max(6, "cannot exceed"),
+	id: Yup.number().required("it is required").max(3, "cannot exceed three"),
+	cardNumber: Yup.number()
+		.required("it is required")
+		.max(16, "cannot exceed three"),
+});
 
-export default PersonalInformationChangeModal
+const PersonalInformationChangeModal = ({
+	visible,
+	onClose,
+}: {
+	visible: boolean;
+	onClose: () => void;
+}) => {
+	const router: Router = useRouter();
+
+	return (
+		<Modal
+			animationType="slide"
+			transparent={true}
+			visible={visible}
+			onRequestClose={onClose}
+		>	
+			
+				<Pressable
+					onPress={onClose}
+					style={styles.modaloverlay}
+				>
+				</Pressable>
+				<View style={styles.container}>
+					<Text style={styles.title}>Update Profile</Text>
+					<Formik
+						initialValues={{
+							name: "",
+							cardNumber: "",
+							expireDate: "",
+							id: "",
+						}}
+						validationSchema={LoginSchema}
+						onSubmit={(values) => console.log(values)}
+						validateOnChange={true}
+					>
+						{({
+							handleChange,
+							handleBlur,
+							handleSubmit,
+							values,
+							errors,
+							touched,
+						}) => (
+							<>
+								<View style={styles.textInputs}>
+									<View
+										style={[
+											styles.textInput,
+											errors.name && touched.name
+												? styles.invalidBox
+												: null,
+										]}
+									>
+										<TextInput
+											placeholder="Name of Card"
+											onChangeText={handleChange("name")}
+											onBlur={handleBlur("name")}
+											value={values.name}
+											style={styles.input}
+										></TextInput>
+										{values.name.length >= 1 &&
+											(errors.name && touched.name ? (
+												<FontAwesome
+													name="times"
+													size={20}
+													color="red"
+													style={styles.inputConfirm}
+												/>
+											) : (
+												<Ionicons
+													name="checkmark-sharp"
+													size={20}
+													color="green"
+													style={styles.inputConfirm}
+												/>
+											))}
+										{errors.name && touched.name ? (
+											<Text
+												style={styles.invalidSignUpText}
+											>
+												{errors.name}
+											</Text>
+										) : null}
+									</View>
+									<View
+										style={[
+											styles.textInput,
+											errors.cardNumber &&
+											touched.cardNumber
+												? styles.invalidBox
+												: null,
+										]}
+									>
+										<TextInput
+											placeholder="Card Number"
+											onChangeText={handleChange(
+												"cardNumber"
+											)}
+											onBlur={handleBlur("cardNumber")}
+											value={values.cardNumber}
+											style={styles.input}
+										></TextInput>
+										{values.cardNumber.length >= 1 &&
+											(errors.cardNumber &&
+											touched.cardNumber ? (
+												<FontAwesome
+													name="times"
+													size={20}
+													color="red"
+													style={styles.inputConfirm}
+												/>
+											) : (
+												<Ionicons
+													name="checkmark-sharp"
+													size={20}
+													color="green"
+													style={styles.inputConfirm}
+												/>
+											))}
+										{errors.cardNumber &&
+										touched.cardNumber ? (
+											<Text
+												style={styles.invalidSignUpText}
+											>
+												{errors.cardNumber}
+											</Text>
+										) : null}
+									</View>
+									<View
+										style={[
+											styles.textInput,
+											errors.expireDate &&
+											touched.expireDate
+												? styles.invalidBox
+												: null,
+										]}
+									>
+										<TextInput
+											placeholder="Expire Date"
+											onChangeText={handleChange(
+												"expireDate"
+											)}
+											onBlur={handleBlur("expireDate")}
+											value={values.expireDate}
+											style={styles.input}
+										></TextInput>
+										{values.expireDate.length >= 1 &&
+											(errors.expireDate &&
+											touched.expireDate ? (
+												<FontAwesome
+													name="times"
+													size={20}
+													color="red"
+													style={styles.inputConfirm}
+												/>
+											) : (
+												<Ionicons
+													name="checkmark-sharp"
+													size={20}
+													color="green"
+													style={styles.inputConfirm}
+												/>
+											))}
+										{errors.expireDate &&
+										touched.expireDate ? (
+											<Text
+												style={styles.invalidSignUpText}
+											>
+												{errors.expireDate}
+											</Text>
+										) : null}
+									</View>
+									<View
+										style={[
+											styles.textInput,
+											errors.id && touched.id
+												? styles.invalidBox
+												: null,
+										]}
+									>
+										<TextInput
+											placeholder="CVV"
+											onChangeText={handleChange("id")}
+											onBlur={handleBlur("id")}
+											value={values.id}
+											style={styles.input}
+										></TextInput>
+										{values.id.length >= 1 &&
+											(errors.id && touched.id ? (
+												<FontAwesome
+													name="times"
+													size={20}
+													color="red"
+													style={styles.inputConfirm}
+												/>
+											) : (
+												<Ionicons
+													name="checkmark-sharp"
+													size={20}
+													color="green"
+													style={styles.inputConfirm}
+												/>
+											))}
+										{errors.id && touched.id ? (
+											<Text
+												style={styles.invalidSignUpText}
+											>
+												{errors.id}
+											</Text>
+										) : null}
+									</View>
+								</View>
+								<View style={styles.button}>
+									<Pressable onPress={() => handleSubmit()}>
+										<Text>ADD CARD</Text>
+									</Pressable>
+								</View>
+							</>
+						)}
+					</Formik>
+				</View>
+			
+		</Modal>
+	);
+};
+
+export default PersonalInformationChangeModal;
