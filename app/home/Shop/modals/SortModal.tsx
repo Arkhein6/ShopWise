17b37d95c6402adcad628c7b5353fd8900edd592
RedStar6 <richmondnkrumah04@gmail.com>
2023-09-29 19:22:32 +0000

    #component: modal for products sorting

---
+++ app/home/Shop/modals/SortModal.tsx
@@ -0,0 +1,40 @@
+import { View, Text, Modal, Pressable } from "react-native";
+import React from "react";
+
+const SortModal = ({
+	visible,
+	onClose,
+}: {
+	visible: boolean;
+	onClose: () => void;
+}) => {
+	return (
+		<Modal
+			visible={visible}
+			onRequestClose={onClose}
+			animationType="slide"
+			transparent={true}
+		>
+			<Pressable onPress={onClose}></Pressable>
+			<View>
+				<Pressable>
+					<Text>Popular</Text>
+				</Pressable>
+				<Pressable>
+					<Text>Newest</Text>
+				</Pressable>
+				<Pressable>
+					<Text>Customer Review</Text>
+				</Pressable>
+				<Pressable>
+					<Text>Price: lowest to high</Text>
+				</Pressable>
+				<Pressable>
+					<Text>Price: highest to low</Text>
+				</Pressable>
+			</View>
+		</Modal>
+	);
+};
+
+export default SortModal;
