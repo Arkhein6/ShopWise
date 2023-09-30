d77d77447297d7640de1343f73e6c5f6230dba68
RedStar6 <richmondnkrumah04@gmail.com>
2023-09-30 23:36:35 +0000

    #correction: fixed ui spacing glitch

--- app/home/Shop/index.tsx
+++ app/home/Shop/index.tsx
@@ -20,119 +20,125 @@ const index = () => {
 	];
 	const [currentCategory, setCurrentCategory] = useState(categories[0]);
 	const [productView, setProductView] = useState<"Grid" | "List">("Grid");
-	const [modalVisible,setModalVisible] = useState<boolean>(false)
+	const [modalVisible, setModalVisible] = useState<boolean>(false);
 
 	const modalHandler = (): void => {
-		setModalVisible((curr) => !curr)
-	}
-
+		setModalVisible((curr) => !curr);
+	};
 
 	return (
 		<View>
-			<View style={{
-				gap:10,
-				backgroundColor:'white'
-			}}>
-			<Text style={styles.maintitle}>Women's Tops</Text>
-			<ScrollView
-				horizontal
-				contentContainerStyle={styles.shopcategoriescontainer}
-				showsHorizontalScrollIndicator={false}
-				style={{
-					paddingLeft:'5%',
-
-				}}
-			>
-				{categories.map((current, idx) => (
-					<Pressable
-						key={idx}
-						onPress={() => {
-							setCurrentCategory(current);
-						}}
-						style={[
-							styles.shopcategories,
-							current === currentCategory
-								? styles.shopcurrentcategory
-								: null,
-						]}
-					>
-						<Text
-							style={
-								current === currentCategory
-									? { color: "white" }
-									: null
-							}
-						>
-							{current}
-						</Text>
-					</Pressable>
-				))}
-				<Link href={"/home/Shop/Categories"} asChild>
-					<Pressable style={styles.allcategoriesbutton}>
-						<Text>View all</Text>
-					</Pressable>
-				</Link>
-			</ScrollView>
-			<View
-				style={{
-					flexDirection: "row",
-					justifyContent:"space-between",
-					paddingTop:5,
-					paddingBottom:5,
-					paddingLeft:'5%',
-					paddingRight:'5%',
-					
-				}}
-			>
-				<>
-					<Pressable
-						style={{
-							flexDirection: "row",
-					alignItems:'center',
-					gap:5
-
-						}}
-					>
-						<Ionicons name="filter-sharp" size={20} color="black" />
-						<Text>Filters</Text>
-					</Pressable>
-				</>
-				<>
-					<Pressable
-						style={{
-							flexDirection: "row",
-							alignItems:'center',
-					gap:5
-
-						}
-					}
-					onPress={modalHandler}
-					>
-						<MaterialCommunityIcons
-							name="swap-vertical"
-							size={24}
-							color="black"
-						/>
-						<Text>Price: lowest to high</Text>
-					</Pressable>
-				</>
-				<Pressable style={{
-				}}
-					onPress={() =>
-						setProductView((currView) =>
-							currView === "Grid" ? "List" : "Grid"
-						)
-					}
+			<View style={{ gap: 10, backgroundColor: "white" }}>
+				<Text style={styles.maintitle}>Women's Tops</Text>
+				<ScrollView
+					horizontal
+					contentContainerStyle={styles.shopcategoriescontainer}
+					showsHorizontalScrollIndicator={false}
+					style={{
+						paddingLeft: "5%",
+					}}
 				>
-					{productView === "Grid" ? (
-						<Ionicons name="grid-sharp" size={24} color="black" />
-					) : (
-						<FontAwesome5 name="th-list" size={24} color="black" />
-					)}
-				</Pressable>
+					{categories.map((current, idx) => (
+						<Pressable
+							key={idx}
+							onPress={() => {
+								setCurrentCategory(current);
+							}}
+							style={[
+								styles.shopcategories,
+								current === currentCategory
+									? styles.shopcurrentcategory
+									: null,
+							]}
+						>
+							<Text
+								style={
+									current === currentCategory
+										? { color: "white" }
+										: null
+								}
+							>
+								{current}
+							</Text>
+						</Pressable>
+					))}
+					<Link href={"/home/Shop/Categories"} asChild>
+						<Pressable style={styles.allcategoriesbutton}>
+							<Text>View all</Text>
+						</Pressable>
+					</Link>
+				</ScrollView>
+				<View
+					style={{
+						flexDirection: "row",
+						justifyContent: "space-between",
+						paddingTop: 5,
+						paddingBottom: 5,
+						paddingLeft: "5%",
+						paddingRight: "5%",
+					}}
+				>
+					<>
+						<Pressable
+							style={{
+								flexDirection: "row",
+								alignItems: "center",
+								gap: 5,
+							}}
+						>
+							<Ionicons
+								name="filter-sharp"
+								size={20}
+								color="black"
+							/>
+							<Text>Filters</Text>
+						</Pressable>
+					</>
+					<>
+						<Pressable
+							style={{
+								flexDirection: "row",
+								alignItems: "center",
+								gap: 5,
+							}}
+							onPress={modalHandler}
+						>
+							<MaterialCommunityIcons
+								name="swap-vertical"
+								size={24}
+								color="black"
+							/>
+							<Text>Price: lowest to high</Text>
+						</Pressable>
+					</>
+					<Pressable
+						style={{}}
+						onPress={() =>
+							setProductView((currView) =>
+								currView === "Grid" ? "List" : "Grid"
+							)
+						}
+					>
+						{productView === "Grid" ? (
+							<Ionicons
+								name="grid-sharp"
+								size={24}
+								color="black"
+							/>
+						) : (
+							<FontAwesome5
+								name="th-list"
+								size={24}
+								color="black"
+							/>
+						)}
+					</Pressable>
+				</View>
 			</View>
-			<SortModal onClose={modalHandler} visible={modalVisible}></SortModal>
-			</View>
+			<SortModal
+				onClose={modalHandler}
+				visible={modalVisible}
+			></SortModal>
 		</View>
 	);
 };
