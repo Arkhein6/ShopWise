bddbf5688a0e788cd8fcdad39803c60dab3870e6
RedStar6 <richmondnkrumah04@gmail.com>
2023-09-30 23:57:44 +0000

    added basic elements

--- app/home/Shop/modals/Filters.tsx
+++ app/home/Shop/modals/Filters.tsx
@@ -3,10 +3,52 @@ import React from "react";
 import { filters as styles } from "./styles";
 
 const Filters = () => {
+    const colors: string[] = ["black","brown","green","blue","pink","red"]
+    const sizes: string[] = ["XS","S","M","L","XL"]
+    const mainCategories: string[] = ["All", "Women","Men","Boys","Women"]
 	return (
 		<View>
 			<ScrollView showsVerticalScrollIndicator={false}>
-				
+				<View>
+                    <Text>Colors</Text>
+                    <ScrollView>
+                        {
+                            colors.map((current) => (
+                                <Pressable>
+                                    <View>
+
+                                    </View>
+                                </Pressable>
+                            ))
+                        }
+                    </ScrollView>
+                </View>
+                <View>
+                    <Text>Sizes</Text>
+                    <View>
+                        {
+                            sizes.map((current) => (
+                                <Pressable>
+                                    <Text>{current}</Text>
+                                </Pressable>
+                            ))
+                        }
+                    </View>
+                </View>
+                <View>
+                    <Text>
+                        Category
+                    </Text>
+                    <View>
+                        {
+                            mainCategories.map((current) => (
+                                <Pressable>
+                                    <Text>{current}</Text>
+                                </Pressable>
+                            ))
+                        }
+                    </View>
+                </View>
 			</ScrollView>
             <View>
                 <Pressable><Text>Discard</Text></Pressable>
