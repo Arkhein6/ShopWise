af3678e414f62643c813c5cbf6a9e07cb8ab245d
RedStar6 <richmondnkrumah04@gmail.com>
2023-10-01 17:33:09 +0000

    #component: added products to favorites modal

---
+++ app/home/Favorites/modals/AddToFavoritesModal.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const AddToFavoritesModal = () => {
+  return (
+    <View>
+      <Text>AddToFavoritesModal</Text>
+    </View>
+  )
+}
+
+export default AddToFavoritesModal
